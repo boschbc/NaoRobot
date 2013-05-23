dfb3883427556518ef21273fed0a30ede7208571
Raoul Harel <r.harel@student.tudelft.nl>
2013-05-23 10:10:40 +0200

    Simulating Goal Server
    Trying to simulate goal server in LaunchDebugger.cs
    Currently throws a SocketException

--- src/MainProgram.cs
+++ src/MainProgram.cs
@@ -15,6 +15,7 @@ namespace Naovigate
         public static readonly int port = 9559;
         public static readonly string localhost = "127.0.0.1";
         public static readonly string nao = "192.168.0.128";
+        public static readonly string nao2 = "192.168.0.125";
 
         // Use this switch to deactivate debugger invocation:
         public static readonly bool useDebugGui = true;
@@ -23,7 +24,7 @@ namespace Naovigate
         {
             ShutDownHook();
             if (useDebugGui)
-                LaunchDebugger.DebugMain(localhost);
+                LaunchDebugger.DebugMain(nao2);
             else
                 Stuff();
         }
