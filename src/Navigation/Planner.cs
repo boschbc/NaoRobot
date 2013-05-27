b7f1b27599ad9554970ac2fdfcd72e33ef8692c8
Mark Cilissen <m.h.j.cilissen@student.tudelft.nl>
2013-05-27 10:54:36 +0200

    Added proper map structure. Added planner skeleton.

---
+++ src/Navigation/Planner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Naovigate.Navigation
+{
+    class Planner
+    {
+        public static void PlanRoute(Map map, List<Point> points)
+        {
+
+        }
+    }
+}
