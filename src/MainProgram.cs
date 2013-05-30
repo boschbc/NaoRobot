ad4d6ea772b4826f69c3bbfe28bc15f9c81b295b
Jorik Oostenbrink <j.oostenbrink@student.tudelft.nl>
2013-05-30 10:15:18 +0200

    merge

--- src/MainProgram.cs
+++ src/MainProgram.cs
@@ -20,8 +20,8 @@ namespace Naovigate
     {
         public static readonly int port = 9559;
         public static readonly string localhost = "127.0.0.1";
-        public static readonly string nao2 = "192.168.0.126";
-        public static readonly string ip = nao2;
+        public static readonly string nao2 = "192.168.0.128";
+        public static readonly string ip = localhost;
 
         public static void Main(String[] args)
         {
@@ -31,7 +31,8 @@ namespace Naovigate
                 LaunchDebugger.DebugMain();
             else
             {
-                GoalCommunicator c = new GoalCommunicator(GoalCommunicator.DefaultIP, 1337);
+                new TestingGoalServer().Start();
+                GoalCommunicator c = new GoalCommunicator(GoalCommunicator.DefaultIP, GoalCommunicator.DefaultPort);
                 c.Start();
             }
 
